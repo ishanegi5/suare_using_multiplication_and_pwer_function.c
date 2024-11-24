4322b1a048fce0cca6be9bd7124564b913dcbf6e
Isha Negi <itsishanegi@gmail.com>
2024-11-24 16:34:21 +0530

    Create square_using_multipliction_and_power_function.c

---
+++ square_using_multipliction_and_power_function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,square;
+    printf("Enter a= ");
+    scanf("%d",&a);
+    square=a*a;
+    printf("Using multiplication the square of %d is : %d\n",a,square);
+    printf("Using power function to get the square of %d is= ",a);
+    printf("%.0f",pow(a,2));
+     printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+}
